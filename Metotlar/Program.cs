0ac4b0a3f6a1762a9f969269cd4ca7e5414ca0d2
Furkan Duman <furkilarus@gmail.com>
2021-01-24 14:37:05 +0300

    20 Ocak Ders Sonu.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya Elması";
+            string[] meyveler = new string[] { };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Çilek";
+            urun2.Fiyati = 10;
+            urun2.Aciklama = "Çilek Gibi Çilek";
+
+            Urun[] urunler = new Urun[] {urun1, urun2 };
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine( "----------------------");
+            }
+
+            Console.WriteLine("-----------------Metotlar---------------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil Armut", 12,12);
+            sepetManager.Ekle2("Elma", "Yeşil Elma", 16,12);
+            sepetManager.Ekle2("Karpuz", "Diyatbakır Karpuzu", 30,12);
+
+
+        }
+    }
+}
